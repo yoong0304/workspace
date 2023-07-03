418ac4caa14a6558119dd398c5d93cb85dc5519d
yoong0304 <yoong0304@naver.com>
2023-07-04 00:52:18 +0900

    0704

--- vscode/hello/src/a0703/Ex7_2.java
+++ vscode/hello/src/a0703/Ex7_2.java
@@ -4,13 +4,42 @@ import java.util.Scanner;
 
 public class Ex7_2 {
     public static void main(String[] args) {
+        boolean run = true;
         int h = 0;
         int m = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("시간을 입력하세요(0~23) : ");
-        h = scanner.nextInt();
-        System.out.println("분을 입력하세요(0~59) : ");
-        m = scanner.nextInt();
 
+        while (run) {
+            System.out.println("시간을 입력하세요(0~23) : ");
+            h = scanner.nextInt();
+            if (h < 0 || h > 23) {
+                System.out.println("시간을 다시 입력하세요.");
+            } else {
+                run = false;
+            }
+        }
+        run = true;
+        while (run) {
+            System.out.println("분을 입력하세요(0~59) : ");
+            m = scanner.nextInt();
+            if (m < 0 || m > 59) {
+                System.out.println("분을 다시 입력하세요.");
+            } else {
+                run = false;
+            }
+        }
+        System.out.println("상근이가 입력한 알람 시간은" + h + "시" + m + "분 이다.");
+
+        // 창영이의 알람 방식
+        m -= 45;
+        if(m < 0) {
+            h--;
+            if (h < 0) {
+                h += 24;
+            }
+            m += 60;
+        }
+        System.out.println("창영이 방식의 알람 시간은" + h + "시" + m + "분 이다.");
     }
+
 }
