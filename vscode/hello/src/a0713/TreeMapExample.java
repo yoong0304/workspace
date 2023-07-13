bf45eaa5ac565d5e2a5907a42c017e56dd5c1c44
yoong0304 <yoong0304@naver.com>
2023-07-13 14:27:23 +0900

    0713

--- vscode/hello/src/a0713/TreeMapExample.java
+++ vscode/hello/src/a0713/TreeMapExample.java
@@ -1,12 +1,9 @@
 package a0713;
 
-// import java.util.*;
-
 import java.util.Map.Entry;
 import java.util.NavigableMap;
 import java.util.Set;
 import java.util.TreeMap;
-
 
 public class TreeMapExample {
     public static void main(String[] args) {
@@ -25,24 +22,25 @@ public class TreeMapExample {
 
         // 정렬된 엔트리를 하나씩 가져오기
         Set<Entry<String, Integer>> entrySet = treeMap.entrySet();
-        for(Entry<String,Integer> entry : entrySet) {
+        for (Entry<String, Integer> entry : entrySet) {
             System.out.println(entry.getKey() + "-" + entry.getValue());
         }
         System.out.println();
 
         // 특정 키에 대한 값 가져오기
+
         Entry<String, Integer> entry = null;
         entry = treeMap.firstEntry();
-        System.out.println("제일 앞 단어 : " + entry.getKey() + "-" + entry.getValue());
+        System.out.println("제일 앞 단어: " + entry.getKey() + "-" + entry.getValue());
         entry = treeMap.lastEntry();
-        System.out.println("제일 뒤 단어 : " + entry.getKey() + "-" + entry.getValue());
+        System.out.println("제일 뒤 단어: " + entry.getKey() + "-" + entry.getValue());
         entry = treeMap.lowerEntry("ever");
-        System.out.println("ever 앞 단어 : " + entry.getKey() + "-" + entry.getValue() + "\n");
+        System.out.println("ever 앞 단어: " + entry.getKey() + "-" + entry.getValue() + "\n");
 
-        // 내림차순 정렬하기
-        NavigableMap<String,Integer> descendingMap = treeMap.descendingMap();
-        Set<Entry<String,Integer>> descendingEntrySet = descendingMap.entrySet();
-        for(Entry<String,Integer> e : descendingEntrySet) {
+        // 내림차순으로 정렬하기
+        NavigableMap<String, Integer> descendingMap = treeMap.descendingMap();
+        Set<Entry<String, Integer>> descendingEntrySet = descendingMap.entrySet();
+        for (Entry<String, Integer> e : descendingEntrySet) {
             System.out.println(e.getKey() + "-" + e.getValue());
         }
         System.out.println();
@@ -50,7 +48,7 @@ public class TreeMapExample {
         // 범위 검색
         System.out.println("[c~h 사이의 단어 검색]");
         NavigableMap<String, Integer> rangeMap = treeMap.subMap("c", true, "h", false);
-        for(Entry<String,Integer> e : rangeMap.entrySet()) {
+        for (Entry<String, Integer> e : rangeMap.entrySet()) {
             System.out.println(e.getKey() + "-" + e.getValue());
         }
     }
